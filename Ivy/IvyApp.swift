d237d08a3db77b4df47b47e951cf03edcb243b97
AlloTheOne <Allo.busy@gmail.com>
2022-12-07 10:06:21 +0300

    Initial Commit

---
+++ Ivy/IvyApp.swift
@@ -0,0 +1,17 @@
+//
+//  IvyApp.swift
+//  Ivy
+//
+//  Created by Alaa Alabdullah on 07/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct IvyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
