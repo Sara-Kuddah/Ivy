0c30bfdfd1b131c160fd8f348329d0cb7ba9bc9e
Nouf <nouf.sul@outlook.com>
2022-12-08 10:12:51 +0300

    setting file

---
+++ Ivy/settings/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  Ivy
+//
+//  Created by nouf on 14/05/1444 AH.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SettingView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingView()
+    }
+}
