31da908ffd8b324ab80f9661cdd9d25f9e3213fc
Nouf <nouf.sul@outlook.com>
2022-12-12 12:55:49 +0300

    setting

--- Ivy/settings/SettingView.swift
+++ Ivy/settings/SettingView.swift
@@ -8,13 +8,93 @@
 import SwiftUI
 
 struct SettingView: View {
+    @State private var Language = true
+    @State private var Mode = false
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
+        VStack{
+            
+            Text("Setting")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.gray)
+                .padding(.bottom, 55.0)
+                .padding(.trailing, 209.0)
+                .opacity(1)
+           
+    VStack(alignment: .leading, spacing: 23){
 
-struct SettingView_Previews: PreviewProvider {
-    static var previews: some View {
-        SettingView()
+              HStack{
+                    Image(systemName: "globe")
+                        .font(.system(size: 20))
+                        .foregroundColor(Color("ourlightgreen"))
+                  
+Toggle(isOn: $Language) {
+    Text("Language")
+                  }
+
+                  
+                    
+                }
+        HStack{
+            Image(systemName: "moon.circle")
+                .font(.system(size: 22))
+                .foregroundColor(Color("ourlightgreen"))
+            Toggle(isOn: $Mode) {
+                Text("Mode")
+            }
+        }
+                 
+        
+        
+        HStack{
+            Image(systemName: "leaf.circle")
+                .font(.system(size: 22))
+                .foregroundColor(Color("ourlightgreen"))
+            Text("About us")
+            
+        }
+        
+
+                HStack{
+                    Image(systemName: "square.and.arrow.up.circle")
+                        .font(.system(size: 22))
+                        .foregroundColor(Color("ourlightgreen"))
+                    Text("Share App")
+                    
+                }
+
+                HStack{
+        Image(systemName: "lock.circle")
+                        .font(.system(size: 22))
+                        .foregroundColor(Color("ourlightgreen"))
+                    Text("Privacy Policy")
+                    
+                }
+  
+                HStack{
+                    
+    Image(systemName: "arrow.left.circle")
+    .font(.system(size: 22))
+    .foregroundColor(Color("ourlightgreen"))
+    Text("Log out")
+                }
+              
+                HStack{
+    Image(systemName: "xmark.bin.circle")
+    .font(.system(size: 22))
+    .foregroundColor(Color("ourred"))
+            Text("Delete Account")
+    .foregroundColor(Color("ourred"))
+                }
+    }.padding(.leading, 35.0)
+   
+            
+        }.padding(.bottom, 200.0)
+            .padding(.trailing, 38.0)
     }
-}
+        struct SettingView_Previews: PreviewProvider {
+            static var previews: some View {
+                SettingView()
+            }
+        }
+    }
