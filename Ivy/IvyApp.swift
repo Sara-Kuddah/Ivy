824774766e473805487b48848eec1453eb37ab4c
Sara-Kuddah <sara-kuddah@hotmail.com>
2022-12-18 11:26:20 +0300

    updating profile page

--- Ivy/IvyApp.swift
+++ Ivy/IvyApp.swift
@@ -11,7 +11,8 @@ import SwiftUI
 struct IvyApp: App {
     var body: some Scene {
         WindowGroup {
-            OnbordingView()
+            MainTabView()
+           // OnbordingView()
         }
     }
 }
